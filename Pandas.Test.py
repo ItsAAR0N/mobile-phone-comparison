cbba6690543dfaf72488547a58401d75bfb0ad0c
ItsAAR0N <aaronshek0@gmail.com>
2021-01-28 01:13:36 +0000

    Additionally...

--- Pandas.Test.py
+++ Pandas.Test.py
@@ -4,7 +4,7 @@ import pandas as p
 # Assign the table of CSV content to a variable, dataFrame
 dataFrame = p.read_csv('mobilephone_table.csv') # Reads CSV file, ensure csv file is in the same folder 
 
-print(dataFrame.head()) # It will print the top 5 lines of the dataFrame, aka CSV file.
-#.tail() will print from top to bottom. 
+n = 10 
+print(dataFrame.head(n)) # It will print the top (first) 10 rows of the dataFrame, aka CSV file.
+#.tail() will print from bottom to top
 
-print("Hello!")
