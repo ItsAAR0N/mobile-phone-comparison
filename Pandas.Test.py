bdbf8cdc955a790e91817b934c183bbdf91b41b1
ItsAAR0N <aaronshek0@gmail.com>
2021-01-28 01:40:34 +0000

    Importing .CSV and small sample of data grabbing

--- Pandas.Test.py
+++ Pandas.Test.py
@@ -16,3 +16,5 @@ cost5000 = dataFrame[(dataFrame['Cost'] == '4,999')]
 print(cost5000.head(n))
 # Will print the first 10 phones
 cost5000.to_csv('cost-5000.csv') # Creates a new CSV with the parameters
+
+# You can also do len() to get no.
