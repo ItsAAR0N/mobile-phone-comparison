ebbdd8c0ec6a892c5b11e9214d09ee399300cb1c
ItsAAR0N <aaronshek0@gmail.com>
2021-01-28 00:44:51 +0000

    Added a little bit on CSV importing

--- Pandas.Test.py
+++ Pandas.Test.py
@@ -1,1 +1,8 @@
-print("Testing 123")
+# Ensure Pandas is installed firstly
+import pandas as pandas
+
+# Assign the table of CSV content to a variable, dataFrame
+dataFrame = pandas.read_csv('mobilephone_table.csv') # Reads CSV file, ensure csv file is in the same folder 
+
+print(dataFrame.head()) # It will print the top 5 lines of the dataFrame, aka CSV file.
+
