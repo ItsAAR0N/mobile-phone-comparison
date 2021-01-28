590205c59d53c85150a5b162083932fc6ab945e7
ItsAAR0N <aaronshek0@gmail.com>
2021-01-28 00:47:48 +0000

    Testing

--- Pandas.Test.py
+++ Pandas.Test.py
@@ -5,4 +5,5 @@ import pandas as pandas
 dataFrame = pandas.read_csv('mobilephone_table.csv') # Reads CSV file, ensure csv file is in the same folder 
 
 print(dataFrame.head()) # It will print the top 5 lines of the dataFrame, aka CSV file.
+#.tail() will print from top to bottom. 
 
