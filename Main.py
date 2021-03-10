59330819058fd80ad1bcc785bfabe29149a1d8cc
ItsAAR0N <aaronshek0@gmail.com>
2021-03-10 02:02:34 +0000

    Progression... (As of 02:00 AM 10/03/21)

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import time as t
 # Assign the table of CSV content to a variable, dataFrame
 dataFrame = p.read_csv(r'C:\Users\aaron\OneDrive - University of Strathclyde\EE106 Engineering Design for Software\Semester 2 project\Repository\mobilephone_table.csv') # Reads CSV file, ensure csv file is in the same folder 
 dataFrame.drop_duplicates(keep='first',inplace=True) # Remove duplicate rows, keeping first set to prevent data loss, inplace means the dataFrame is updated along
+dataFrame.dropna(inplace=True) # Drops any NaN values or (empty) cells, inplace to keep changes permanent 
 
 print("\nHello, welcome to the Mobile Phone Comparison.")
 print("There are in total: {0} phones.".format(len(dataFrame))) # Prints total number of rows and columns
@@ -28,7 +29,9 @@ class Filtering:
             except ValueError: # If value is not int or is zero
                 print("Must be int.")
         for i in range(1,dropCount+1): # Add one to compensate 
-            dropSelection = input("Please enter column {0} you want to remove: ".format(i)) # Ensures readability e.g 1... 2.. etc.
+            dropSelection = ""
+            while dropSelection not in list(self.dataFrame): # Input validation for when user input =/= to any column headings            
+                dropSelection = input("Please enter column {0} you want to remove: ".format(i)) # Ensures readability e.g 1... 2.. etc.
             self.dataFrame.drop(columns=[dropSelection],inplace=True) # Start to drop coloumn(s), the use of inplace allows for modification of the dataframe without having to re-assign it             
         print(self.dataFrame) # Show the most up-to-date table after alteration(s) 
 
@@ -41,13 +44,13 @@ class Filtering:
             while AscendingInput != "y" and AscendingInput != "n":
                 AscendingInput = input("\n(Please re-enter) [Y] for ascending order, [N] for descending order: ").lower()
         if AscendingInput == "y": # Will be ascending
-            AscendingOrder = 0
+            AscendingOrder = False
         elif AscendingInput == "n": # Will be descending
-            AscendingOrder = 1
+            AscendingOrder = True
         self.dataFrame.sort_values(by=[ColumnSelected],ascending=AscendingOrder,inplace=True) # Now sort according to user chosen options
         print(self.dataFrame)
 
-    # NEARLY COMPLETED
+    # COMPLETED
     def narrowSearch(self):
         UserChoice = input("\nWould you like to narrow (delete) your searches based on:\n\n[A] Phone Name\n[B] Cost\n\nEnter choice: ").lower() # Prompt user for desired services
         while UserChoice != "a" and UserChoice != "b": # Input validation
@@ -62,7 +65,7 @@ class Filtering:
             phonecostChoice = input("\nPlease enter the cost: ") # Set desired phone price
             costNarrowChoice = input("\nWould you like to narrow (delete) phones that cost {0} [A], less than [B], or greater than? [C]: ".format(phonecostChoice)).lower() # Choice to choose between equal to, less than or greater than their cost input
             while costNarrowChoice != "a" and costNarrowChoice != "b" and costNarrowChoice != "c":
-                costNarrowChoice = input("\n(Error) must be either A for same cost, B for less than or C for greater than:").lower()
+                costNarrowChoice = input("\n(Error) must be either A for same cost, B for less than or C for greater than: ").lower()
             if costNarrowChoice == "a":
                 self.dataFrame.drop(self.dataFrame[dataFrame.Cost == phonecostChoice].index, inplace=True) # Drops phones that cost phonecostChoice, inplace ensures table is constantly updated
                 print(self.dataFrame)
@@ -73,19 +76,32 @@ class Filtering:
                 self.dataFrame.drop(self.dataFrame[dataFrame.Cost >= phonecostChoice].index, inplace=True)
                 print(self.dataFrame)
                 
-
-    def specificSearch(self): # An alternative rather than going through all of the above
-        print("Hello") # WIP - user can explicity search for specific phones in one go.
+    def specificSearch(self): # An alternative rather than going through all of the above        
+        columnHeadings = ['Name','Storage_details','Camera_details','Battery_details','Processor','Cost']
+        print("Please select a phone based on its:\n{0}".format(', '.join(list(columnHeadings))))
+        # Define local var 
+        columnTerm = ""
+        while columnTerm not in columnHeadings: # Input validation for when user input =/= to any column headings
+            columnTerm = input("Please enter desired column here: ")            
+        searchTerm = input("Please now enter the item you wish to explicitly search within {0}: ".format(columnTerm))
+        self.dataFrame.drop(columns=['ImageUrl'],inplace=True)
+        self.dataFrame = self.dataFrame[self.dataFrame[columnTerm].str.contains(searchTerm)] # If the row contains a phone that the user has specified, it shown
+        print(self.dataFrame)
 
     # COMPLETED
     def initial(self):
         print("\nPlease select the desired function by its number: ")
         print("\n[1] Drop columns \n[2] Sort in alph./number order \n[3] Narrow Search\n[4] Specific Search") # Display list of options to choose from
-        # WIP ABOVE (WILL ADD MORE OPTIONS)
-        user_input = "" # Define local variables 
+    # WIP ABOVE (WILL ADD MORE OPTIONS)
+        user_input = 0 # Define local variables 
         # Input validation (USING WHILE)
-        while user_input == "" or user_input < 1 or user_input > 4:
-            user_input = int(input("\nEnter here: "))
+        while True:
+            try:
+                while user_input < 1 or user_input > 5: # Detects if user_input is within range (1,5)
+                    user_input = int(input("\nEnter here: "))
+                break
+            except ValueError: # If user_input is not an integer it will be detected (ValueError)
+                print("Error must be int.")
         if user_input == 1: # Drop columns
             self.dropColumn()
             self.repeat() # Allows for repeatadability functionality 
@@ -97,22 +113,29 @@ class Filtering:
             self.repeat()
         elif user_input == 4: # Specific search
             self.specificSearch()
-            self.repeat()
+            self.repeat()    
+        elif user_input == 5:  # HIDDEN/TEMPORARY (EXIT FOR PROGRAM TESTING PURPOSES)
+            self.testingExit() 
 
     # WIP                            
     def repeat(self): # Program repeatability 
+        # Define local var
         userRequest = ""
-        while userRequest == "" and userRequest != "y" and userRequest != "n": # Input validation
+        # Input validation (makes sure user enters y/n only)
+        while userRequest != "y" and userRequest != "n":
             userRequest = input("Would you like to further narrow/filter results? [Y/N]: ").lower()
         if userRequest == "y":
             self.initial()
         elif userRequest == "n":
             print(self.dataFrame)
-            print("\nHave a nice day! :)")
+            print("\nHave a nice day! :)") 
             t.sleep(10)
-            raise SystemExit
-
+            raise SystemExit # Shuts down program with 10 second wait before      
         # WIP
+
+    # TEMPORARY (EXIT FOR PROGRAM TESTING PURPOSES)
+    def testingExit(self):
+        raise SystemExit
 
 # CLASS COMPARISON (COMPARE TWO DATAFRAMES)
 class Comparison:
