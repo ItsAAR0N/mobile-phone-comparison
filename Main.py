3e2e578bc36adafb1bc85dafbd02f828044b695d
ItsAAR0N <aaronshek0@gmail.com>
2021-03-14 00:01:03 +0000

    Fixed bugs/errors + Big Improvements (PROGRAM FULLY WORKING AS OF NOW) + start of Tkinter

--- Main.py
+++ Main.py
@@ -1,30 +1,29 @@
 # Import of libraries 
 import pandas as p
-import tkinter 
+from tkinter import filedialog
+from tkinter import Tk
 import time as t
-
-# INITIAL DATAFRAME PARSING
-# Assign the table of CSV content to a variable, dataFrame
-
+# MAKE SURE YOU RUN PYTHON AS ADMIN!
+# Assign the table of CSV content to a variable, dataFrame then pass to class
 dataFrame = p.read_csv(r'C:\Users\aaron\OneDrive - University of Strathclyde\EE106 Engineering Design for Software\Semester 2 project\Repository\mobilephone_table.csv') # Reads CSV file, ensure csv file is in the same folder 
-dataFrame.drop_duplicates(keep='first',inplace=True) # Remove duplicate rows, keeping first set to prevent data loss, inplace means the dataFrame is updated along
-dataFrame.dropna(inplace=True) # Drops any NaN values or (empty) cells, inplace to keep changes permanent 
-dataFrame.drop(dataFrame.index[24], inplace=True) # Removes the one extra heading in posiiton 24
-dataFrame['Cost'] = dataFrame.Cost.str.split(',').str.join('') # Splits the cost to integer rather than string with comma separated values
 
-print("\nHello, welcome to the Mobile Phone Comparison.")
-print("There are in total: {0} phones.".format(len(dataFrame))) # Prints total number of rows and columns
+print("\nHello, welcome to the Mobile Phone Comparison!")
+root = Tk() # All TK modules require a root at the beginning as per standardized measures
+root.withdraw() # Need not full GUI, so root window is withdrawn
 
 # CLASS FILTERING (MAIN)
 class Filtering: 
-    def __init__(self): # Initialise attributes of the class # VarOne, VarTwo
-       self.dataFrame = dataFrame # Import set of data
-       #self.VarOne = VarOne # WIP
-       #self.VarTwo = VarTwo
+    def __init__(self): # Initialise attributes of the class
+        self.dataFrame = dataFrame # Import set of data
+        # INITIAL DATAFRAME PARSING
+        self.dataFrame.drop_duplicates(keep='first',inplace=True) # Remove duplicate rows, keeping first set to prevent data loss, inplace means the dataFrame is updated along
+        self.dataFrame.dropna(inplace=True) # Drops any NaN values or (empty) cells, inplace to keep changes permanent 
+        self.dataFrame.drop(dataFrame.index[24], inplace=True) # Removes the one extra heading in position 24
+        self.dataFrame['Cost'] = dataFrame.Cost.str.split(',').str.join('').astype(int) # Splits the cost to integer rather than string with commas
 
     # COMPLETED        
     def dropColumn(self): 
-        print("\nThe column headings are as followed:\n{0}".format(', '.join(list(dataFrame)))) # Displays the column headings which the user can choose to remove 
+        print("\nThe column headings are as followed:\n{0}".format(', '.join(list(self.dataFrame)))) # Displays the column headings which the user can choose to remove 
         # Which is formatted to suit the print expression followed by seperation of ', ' then joins each column name together.
         while True: # Input validation
             try:
@@ -35,14 +34,14 @@ class Filtering:
         for i in range(1,dropCount+1): # Add one to compensate 
             dropSelection = ""
             while dropSelection not in list(self.dataFrame): # Input validation for when user input =/= to any column headings            
-                dropSelection = input("Please enter column {0} you want to remove: ".format(i)) # Ensures readability e.g 1... 2.. etc.
+                dropSelection = input("Please enter column {0} you want to remove (Case sensitive): ".format(i)) # Ensures readability e.g 1... 2.. etc.
             self.dataFrame.drop(columns=[dropSelection],inplace=True) # Start to drop coloumn(s), the use of inplace allows for modification of the dataframe without having to re-assign it             
         print(self.dataFrame) # Show the most up-to-date table after alteration(s) 
 
     # COMPLETED     
     def sortBy(self): # Add alpha./numerical order sorting functionality
+        print("\nThe column headings are as followed:\n{0}".format(', '.join(list(self.dataFrame)))) # Displays the column headings which the user can choose to remove 
         ColumnSelected,AscendingInput = "",True
-        print("\nThe column headings are as followed:\n{0}".format(', '.join(list(dataFrame)))) # Displays the column headings which the user can choose to remove 
         while ColumnSelected == "" and AscendingInput == True: # Input Validation
             ColumnSelected,AscendingInput = input("\nPlease enter a column to sort in order: "),  input("\n[Y] for ascending order, [N] for descending order: ").lower()
             while ColumnSelected not in list(dataFrame):
@@ -53,25 +52,32 @@ class Filtering:
             AscendingOrder = True
         elif AscendingInput == "n": # Will be descending
             AscendingOrder = False
-        self.dataFrame.sort_values(ColumnSelected,ascending=AscendingOrder,inplace=True) # Now sort according to user chosen options
+        self.dataFrame.sort_values(by=[ColumnSelected],ascending=AscendingOrder,inplace=True) # Now sort according to user chosen options
         print(self.dataFrame)
 
     # COMPLETED
     def narrowSearch(self):
-        UserChoice = input("\nWould you like to narrow (delete) your searches based on:\n\n[A] Phone Name\n[B] Cost\n\nEnter choice: ").lower() # Prompt user for desired services
+        UserChoice = ""
         while UserChoice != "a" and UserChoice != "b": # Input validation
-            UserChoice = input("\n(Error) must be either A for Phone Name or B or Phone Cost: ").lower()
+            UserChoice = input("\nWould you like to narrow (delete) your searches based on:\n\n[A] Phone Name\n[B] Cost\n\nEnter choice: ").lower() # Prompt user for desired services
+
         if UserChoice == "a":
             phonenameChoice = input("\nPlease enter the phone name: ") # Set desired phone name
-            self.dataFrame = self.dataFrame[~self.dataFrame.Name.str.contains(phonenameChoice)] # If the row contains a phone that the user has specified, it will be deleted
-            # Unable to use inplace=True in this case as it has to be defined.
+            self.dataFrame = self.dataFrame[~self.dataFrame.Name.str.contains(phonenameChoice,case=False)] # If the row contains a phone that the user has specified, it will be deleted
+            # Unable to use inplace=True in this case as it has to be defined. Case=False means that the input need not case sensitive
             print(self.dataFrame)
 
         elif UserChoice == "b": # User can narrow according to cost paramater
-            phonecostChoice = input("\nPlease enter the cost: ") # Set desired phone price
-            costNarrowChoice = input("\nWould you like to narrow (delete) phones that cost {0} [A], less than [B], or greater than? [C]: ".format(phonecostChoice)).lower() # Choice to choose between equal to, less than or greater than their cost input
+            # Input Validation
+            while True:
+                try:
+                    phonecostChoice = int(input("\nPlease enter the cost: ")) # Set desired phone price
+                    break
+                except ValueError:
+                    print("Error, must be int.")  
+            costNarrowChoice = ""                 
             while costNarrowChoice != "a" and costNarrowChoice != "b" and costNarrowChoice != "c":
-                costNarrowChoice = input("\n(Error) must be either A for same cost, B for less than or C for greater than: ").lower()
+                costNarrowChoice = input("\nWould you like to narrow (delete) phones that cost {0} [A], less than [B], or greater than? [C]: ".format(phonecostChoice)).lower() # Choice to choose between equal to, less than or greater than their cost input
             if costNarrowChoice == "a":
                 self.dataFrame.drop(self.dataFrame[dataFrame.Cost == phonecostChoice].index, inplace=True) # Drops phones that cost phonecostChoice, inplace ensures table is constantly updated
                 print(self.dataFrame)
@@ -83,23 +89,21 @@ class Filtering:
                 print(self.dataFrame)
                 
     def specificSearch(self): # An alternative rather than going through all of the above        
-        columnHeadings = ['Name','Storage_details','Camera_details','Battery_details','Processor','Cost']
+        columnHeadings = ['Name','Storage_details','Camera_details','Battery_details','Processor']
         print("Please select a phone based on its:\n{0}".format(', '.join(list(columnHeadings))))
-        # Define local var 
         columnTerm = ""
         while columnTerm not in columnHeadings: # Input validation for when user input =/= to any column headings
-            columnTerm = input("Please enter desired column here: ")            
+           columnTerm = input("Please enter desired column here (Case sensitive): ")        
         searchTerm = input("Please now enter the item you wish to explicitly search within {0}: ".format(columnTerm))
-        self.dataFrame = self.dataFrame[self.dataFrame[columnTerm].str.contains(searchTerm)] # If the row contains a phone that the user has specified, it shown
+        self.dataFrame = self.dataFrame[self.dataFrame[columnTerm].str.contains(searchTerm,case=False)] # If the row contains a phone that the user has specified, it shown
         print(self.dataFrame)
 
     # COMPLETED
     def initial(self):
         print("\nPlease select the desired function by its number: ")
         print("\n[1] Drop columns \n[2] Sort in alph./number order \n[3] Narrow Search\n[4] Specific Search") # Display list of options to choose from
-    # WIP ABOVE (WILL ADD MORE OPTIONS)
-        user_input = 0 # Define local variables 
         # Input validation (USING WHILE)
+        user_input = 0
         while True:
             try:
                 while user_input < 1 or user_input > 5: # Detects if user_input is within range (1,5)
@@ -107,26 +111,30 @@ class Filtering:
                 break
             except ValueError: # If user_input is not an integer it will be detected (ValueError)
                 print("Error must be int.")
-        if user_input == 1: # Drop columns
+        # Drop columns
+        if user_input == 1: 
             self.dropColumn()
-            self.repeat() # Allows for repeatadability functionality 
-        elif user_input == 2: # Sort in alph./number order
+            self.repeat() # Allows for repeatadability functionality
+        # Sort in alph./number order
+        elif user_input == 2: 
             self.sortBy()
             self.repeat()
-        elif user_input == 3: # Narrow searches
+        # Narrow searches        
+        elif user_input == 3: 
             self.narrowSearch()
             self.repeat()
-        elif user_input == 4: # Specific search
+        # Specific search
+        elif user_input == 4: 
             self.specificSearch()
             self.repeat()    
-        elif user_input == 5:  # HIDDEN/TEMPORARY (EXIT FOR PROGRAM TESTING PURPOSES)
+        # HIDDEN/TEMPORARY (EXIT FOR PROGRAM TESTING PURPOSES)
+        elif user_input == 5:  
             self.testingExit() 
 
     # WIP                            
     def repeat(self): # Program repeatability 
-        # Define local var
-        userRequest = ""
         # Input validation (makes sure user enters y/n only)
+        userRequest = ""
         while userRequest != "y" and userRequest != "n":
             userRequest = input("Would you like to further narrow/filter results? [Y/N]: ").lower()
         # Program will continue re-iterating the functions until the user is finished and enters n 
@@ -134,13 +142,15 @@ class Filtering:
             self.initial()
         elif userRequest == "n":
             print(self.dataFrame) # Print dataFrame state at the moment in time
-            # Define local var
-            downloadResults = "" 
             # Input validation
+            downloadResults = ""
             while downloadResults != "y" and downloadResults != "n":
                 downloadResults = input("Would you like a copy of the dataframe in Excel file format? [Y/N]: ").lower()
                 if downloadResults == "y": # Prints a copy of the dataFrame to Excel format, path is explicitly defined for now - desktop
-                    self.dataFrame.to_csv(r'C:\Users\aaron\Desktop\FilteredMobileList.csv', index=False) # No need for numbering down the left side hence index = False
+                    filepath = r'C:\Users\aaron\Desktop\FilteredMobileList.csv' # Temporary, r stands for "raw string"
+                    # filedialog.askdirectory() # Shows explorer for user to select file directory                                        
+                    self.dataFrame.to_csv(filepath, index=False) # No need for numbering down the left side hence index = False
+                    print("Saved to {0}, have a nice day!".format(filepath))
                 else: # If user does not want to print it will exit.
                     print("\nHave a nice day! :)") 
                     t.sleep(10)
